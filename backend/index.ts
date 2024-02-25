39ceb105c63749aecf584084e49fd93f9d01efb2
usFer123 <151100756+usFer123@users.noreply.github.com>
2024-02-24 21:11:41 -0600

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,22 @@ import { Server, ic } from 'azle';
 import cors from "cors";
 import express from 'express';
 
+type Movi = {
+    id: number;
+    Name: string;
+    Male: string;
+    Study: string;
+    Year:  number;
+}
+
+let Movies: Movi[] = [{
+    id: 1,
+    Name: 'El chico y La Garza',
+    Male: 'Hayao Miyazaki https',
+    Study: 'Studio Ghibli',
+    Year: 2023 
+}];
+
 export default Server(() => {
     const app = express();
 
@@ -17,18 +33,46 @@ export default Server(() => {
     //     }
     // });
 
-    app.post('/test', (req, res) => {
-        res.json(req.body);
+    // GET
+    app.get('/Movies', (req, res) =>{
+        res.json(Movies);
     });
 
-    app.get('/whoami', (req, res) => {
-        res.statusCode = 200;
-        res.send(ic.caller());
+   //POST
+    app.post('/Movies', (req, res) => {
+        const id = req.body.id;
+        const Movi = Movies.find((Movi) => Movi.id === id);
+
+        if(!Movi){
+            Movies = [...Movies, req.body]
+        res.send("ok");
+        }else{
+            res.status(404).send("el id ya existe");
+            return;
+        }
+        
     });
 
-    app.get('/health', (req, res) => {
-        res.send().statusCode = 204;
+    //PUT
+     app.put("/Movies/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const Movi = Movies.find((Movi) => Movi.id === id);
+
+    if(!Movi){
+        res.status(404).send("Valio");
+        return;
+    }
+    const updateBook = { ...Movi, ...req.body };
+    Movies = Movies.map((b) => b.id === updateBook.id ? updateBook : b);
+
+    res.send("ok");
     });
 
+    //DELETE
+    app.delete("/Movies/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    Movies = Movies.filter((Movi) => Movi.id !== id);
+    res.send("ok");
+    });
     return app.listen();
-});
+    });
